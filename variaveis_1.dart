501203db5d5cad519a135e53bc1a2d239b533013
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-12-16 19:20:17 -0300

    Variables and constants

---
+++ variaveis_1.dart
@@ -0,0 +1,26 @@
+main() {
+  // Valores numéricos
+  int a = 2;
+  double b = 3.1314;
+  num c = 3.14; // ambos 'int' e 'double'
+  num d = 4; // ambos 'int' e 'double'
+
+  // Valor do tipo String
+  String greeting = 'Hey! Sound good?';
+
+  // (var) Pelo valor atribuido o 'Dart' vai interpretar.
+  var name = 'Paulo';
+  var n1 = 1;
+  var n2 = 2.5;
+  var n3 = true;
+
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(name);
+  print(greeting);
+  print(n1);
+  print(n2);
+  print(n3);
+}
