1c66751c7b67459ae0de53dfd463f030bc006140
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-12-16 18:48:27 -0300

    Learning dart

---
+++ exercise1.dart
@@ -0,0 +1,19 @@
+main() {
+  num numerosEmGeral1 = 35; // Both: int and double
+  num numerosEmGeral2 = 35.5; // Both: int and double
+
+  int inteiro = 35;
+  double pontoFlutuante = 9.5;
+
+  String name = "Paulo";
+  int age = 35;
+
+  // print(numerosEmGeral1);
+  // print(numerosEmGeral2);
+  // print(inteiro);
+  // print(pontoFlutuante);
+  // print(name);
+
+  print("My name is " + name + " and I'm  years old.");
+  // print("My name is $name and I'm $age years old.");
+}
